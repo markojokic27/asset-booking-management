b37f7b6ec6843597383e0485b8f65e2c8f3b454e
Marko Jokic <jokicmarko98@gmail.com>
2026-03-24 23:56:38 +0100

    add Layout example

--- frontend/src/app/App.tsx
+++ frontend/src/app/App.tsx
@@ -1,31 +1,96 @@
 import { Layout, LayoutRow, LayoutColumn } from '../components/layout/Layout';
 function App() {
+  const arr = Array.from({ length: 12 }, (_, i) => i);
+  const boxClass =
+    'mb-8 flex h-32 items-center justify-center rounded-lg bg-gray-200 text-2xl font-bold';
   return (
     <>
-      <p className="m-4 mt-2 text-center text-4xl font-bold">Asset Manager</p>
-      <p className="m-4 mt-0 text-center text-lg text-gray-700">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro est,
-        maiores nam enim qui tenetur corrupti sunt eveniet nihil blanditiis
-        expedita, minus pariatur accusamus eaque quas! Pariatur consectetur
-        accusantium est.
-      </p>
-      <Layout>
+      <Layout className="bg-yellow-300">
         <LayoutRow>
-          <LayoutColumn mdSpan={6} lgSpan={4}>
-            <p className="mt-2 text-center text-lg md:text-left">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              est, maiores nam enim qui tenetur corrupti sunt eveniet nihil
-              blanditiis expedita, minus pariatur accusamus eaque quas! Pariatur
-              consectetur accusantium est.
-            </p>
+          <LayoutColumn>
+            <h1 className="my-8 text-center text-5xl font-bold">
+              Asset manager
+            </h1>
           </LayoutColumn>
-          <LayoutColumn mdSpan={6} lgSpan={4}>
-            <p className="mt-2 text-center text-lg">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro
-              est, maiores nam enim qui tenetur corrupti sunt eveniet nihil
-              blanditiis expedita, minus pariatur accusamus eaque quas! Pariatur
-              consectetur accusantium est.
-            </p>
+        </LayoutRow>
+      </Layout>
+      <Layout>
+        <LayoutRow className="mt-8">
+          {arr.map((i) => (
+            <LayoutColumn key={i} span={1}>
+              <div className={boxClass}>{i + 1}</div>
+            </LayoutColumn>
+          ))}
+        </LayoutRow>
+        <LayoutRow>
+          <LayoutColumn span={12}>
+            <hr className="mt-8 mb-16" />
+
+            <div className={boxClass}>span=12</div>
+          </LayoutColumn>
+          <LayoutColumn span={6}>
+            <div className={boxClass}>span=6</div>
+          </LayoutColumn>
+          <LayoutColumn span={6}>
+            <div className={boxClass}>span=6</div>
+          </LayoutColumn>
+          <LayoutColumn span={4}>
+            <div className={boxClass}>span=4</div>
+          </LayoutColumn>
+          <LayoutColumn span={4}>
+            <div className={boxClass}>span=4</div>
+          </LayoutColumn>
+          <LayoutColumn span={4}>
+            <div className={boxClass}>span=4</div>
+          </LayoutColumn>
+          <LayoutColumn span={3}>
+            <div className={boxClass}>span=3</div>
+          </LayoutColumn>
+          <LayoutColumn span={3}>
+            <div className={boxClass}>span=3</div>
+          </LayoutColumn>
+          <LayoutColumn span={3}>
+            <div className={boxClass}>span=3</div>
+          </LayoutColumn>
+          <LayoutColumn span={3}>
+            <div className={boxClass}>span=3</div>
+          </LayoutColumn>
+        </LayoutRow>
+        <LayoutRow>
+          <LayoutColumn>
+            <hr className="mt-8 mb-16" />
+          </LayoutColumn>
+          <LayoutColumn span={4}>
+            <div className={boxClass}>span=4</div>
+          </LayoutColumn>
+          <LayoutColumn offset={3} span={5}>
+            <div className={boxClass}>span=5 offset=3</div>
+          </LayoutColumn>
+          <LayoutColumn span={2}>
+            <div className={boxClass}>span=2</div>
+          </LayoutColumn>
+          <LayoutColumn span={2}>
+            <div className={boxClass}>span=2</div>
+          </LayoutColumn>
+          <LayoutColumn offset={2} span={6}>
+            <div className={boxClass}>span=6 offset=2</div>
+          </LayoutColumn>
+        </LayoutRow>
+        <LayoutRow>
+          <LayoutColumn>
+            <hr className="mt-8 mb-16" />
+          </LayoutColumn>
+          <LayoutColumn smSpan={12} mdSpan={6} lgSpan={4} xlSpan={3}>
+            <div className={boxClass}>Responsive column</div>
+          </LayoutColumn>
+          <LayoutColumn smSpan={12} mdSpan={6} lgSpan={4} xlSpan={3}>
+            <div className={boxClass}>Responsive column</div>
+          </LayoutColumn>
+          <LayoutColumn smSpan={12} mdSpan={6} lgSpan={4} xlSpan={3}>
+            <div className={boxClass}>Responsive column</div>
+          </LayoutColumn>
+          <LayoutColumn smSpan={12} mdSpan={6} lgSpan={4} xlSpan={3}>
+            <div className={boxClass}>Responsive column</div>
           </LayoutColumn>
         </LayoutRow>
       </Layout>
