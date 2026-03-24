b5b97636926a27c0526f834a71dc3b4a91ae795d
Nikolina Carevic <ncarevic231@gmail.com>
2026-03-24 10:14:05 +0100

    added types.ts for assets

--- frontend/src/features/assets/types.ts
+++ frontend/src/features/assets/types.ts
@@ -1,1 +1,20 @@
-let a;
+export type Asset = {
+  id: string;
+
+  name: string;
+  description?: string;
+
+  code?: string;
+  location?: string;
+
+  status: string;  // ili neki enum?
+
+  categoryId: string;
+
+  createdDate: Date;
+  lastModifiedDate: Date;
+};
+
+export type AssetDto = Asset & {
+  categoryName?: string;
+};
