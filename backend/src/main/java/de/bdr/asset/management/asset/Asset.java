01cf1d4fc4bda25f6d7909d562ec612a2d48d881
Andela <amjogun@gmail.com>
2026-03-24 14:33:15 +0100

    Asset Entity

--- backend/src/main/java/de/bdr/asset/management/asset/Asset.java
+++ backend/src/main/java/de/bdr/asset/management/asset/Asset.java
@@ -1,4 +1,38 @@
 package de.bdr.asset.management.asset;
 
+
+@Entity
+@Table (name = "asset")
+@Getter
+@Setter
+@Builder
 public class Asset {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long Id
+
+    @NotBlank(message="Name is required")
+    @Size(max=100, message="Name cannot exceed 100 characters")
+    @Column(nullable=false)
+    private String name;
+
+    @Size(max=255, message="Description cannot exceed 255 characters")
+    @Column(columnDefinition="TEXT")
+    private String description;
+
+    @NotNull(message = "Status is required")
+    @Pattern(regexp = "", message = "Status must be ")
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatusEnum status;
+
+    
+
+    @CreationTimestamp
+    @Column(updatable=false)
+    private LocalDateTime createdTimestamp;
+
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedTimestamp;
+
 }
