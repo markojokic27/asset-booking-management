6f130993bafc7e378b49f992c8703dbd6e823df8
Josko Diko <josdjiko@student-gitlab.tn.internal>
2026-03-24 15:35:01 +0100

    Imports for asset

--- backend/src/main/java/de/bdr/asset/management/asset/Asset.java
+++ backend/src/main/java/de/bdr/asset/management/asset/Asset.java
@@ -1,5 +1,16 @@
 package de.bdr.asset.management.asset;
 
+import de.bdr.asset.management.user.UserStatusEnum;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table (name = "asset")
@@ -7,9 +18,10 @@ package de.bdr.asset.management.asset;
 @Setter
 @Builder
 public class Asset {
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private Long Id
+    private Long Id;
 
     @NotBlank(message="Name is required")
     @Size(max=100, message="Name cannot exceed 100 characters")
@@ -26,13 +38,10 @@ public class Asset {
     @Enumerated(EnumType.STRING)
     private UserStatusEnum status;
 
-
-
     @CreationTimestamp
     @Column(updatable=false)
     private LocalDateTime createdTimestamp;
 
     @UpdateTimestamp
     private LocalDateTime lastModifiedTimestamp;
-
 }
