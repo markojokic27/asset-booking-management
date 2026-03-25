1f012778c217bf08e4884bda575a39fdca724f37
Josko Diko <josdjiko@student-gitlab.tn.internal>
2026-03-25 09:50:56 +0100

    Comments added to domain models

--- backend/src/main/java/de/bdr/asset/management/asset/Asset.java
+++ backend/src/main/java/de/bdr/asset/management/asset/Asset.java
@@ -2,8 +2,8 @@ package de.bdr.asset.management.asset;
 
 import de.bdr.asset.management.assetcategory.AssetCategory;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
@@ -18,42 +18,49 @@ import java.time.LocalDateTime;
 @Table(name = "asset")
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Asset {
 
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    /** Name of asset */
     @NotBlank(message="Name is required")
     @Size(max=100, message="Name cannot exceed 100 characters")
     @Column(nullable=false)
     private String name;
 
+    /** Description of asset */
     @Size(max=255, message="Description cannot exceed 255 characters")
     @Column(columnDefinition="TEXT")
     private String description;
 
+    /** Asset Status */
     @NotNull(message = "Status is required")
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private AssetStatusEnum assetStatus;
 
+    /** ID of asset category, foreign key */
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "category_id", nullable = false)
     private AssetCategory category;
 
+    /** Location of asset */
     @NotBlank(message="Location is required")
     @Size(max=255, message="Location cannot exceed 255 characters")
     @Column(nullable=false)
     private String location;
 
+    /** Created at */
     @CreationTimestamp
     @Column(updatable=false)
     private LocalDateTime createdAt;
 
+    /** Last Modified at */
     @UpdateTimestamp
     private LocalDateTime lastModifiedAt;
 }
--- backend/src/main/java/de/bdr/asset/management/assetcategory/AssetCategory.java
+++ backend/src/main/java/de/bdr/asset/management/assetcategory/AssetCategory.java
@@ -1,26 +1,14 @@
 package de.bdr.asset.management.assetcategory;
 
-import java.time.LocalDateTime;
-
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import java.time.LocalDateTime;
 
 /**
  * Asset category domain-entity model.
@@ -33,31 +21,38 @@ import lombok.Setter;
 @AllArgsConstructor
 @Builder
 public class AssetCategory {
+
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
 
+    /** Name of asset category */
     @NotBlank(message="Name is required")
     @Size(max=100, message="Name cannot exceed 100 characters")
     @Column(nullable=false)
     private String name;
 
+    /** Description of asset category */
     @Size(max=255, message="Description cannot exceed 255 characters")
     @Column(columnDefinition="TEXT")
     private String description;
 
+    /** Period of booking */
     @NotNull(message="Booking period is required")
     @Column(nullable=false)
     @Enumerated(EnumType.STRING)
     private BookingPeriodEnum bookingPeriod;
 
+    /** Approval for asset category */
     @Column(nullable = false)
     private boolean approval;
 
+    /** Created at */
     @CreationTimestamp
     @Column(updatable=false)
     private LocalDateTime createdAt;
 
+    /** Last Modified at */
     @UpdateTimestamp
     private LocalDateTime lastModifiedAt;
 }
