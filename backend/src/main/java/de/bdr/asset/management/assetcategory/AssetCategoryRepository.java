c51d1002d6f11ee0cca613d6af6faacb3bc669a4
jakpetri <jakov.petric.student@gmail.com>
2026-03-26 15:47:41 +0100

    Prototype for AssetCategory Repo, Service and Controller.

---
+++ backend/src/main/java/de/bdr/asset/management/assetcategory/AssetCategoryRepository.java
@@ -0,0 +1,7 @@
+package de.bdr.asset.management.assetcategory;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssetCategoryRepository extends JpaRepository<AssetCategory, Long> {
+
+}
---
+++ backend/src/main/java/de/bdr/asset/management/assetcategory/AssetCategoryService.java
@@ -0,0 +1,18 @@
+package de.bdr.asset.management.assetcategory;
+
+import java.util.List;
+
+public interface AssetCategoryService {
+    // CREATE
+    AssetCategoryDTO createAssetCategory(AssetCategoryDTO assetCategoryRequest);
+
+    // READ
+    AssetCategoryDTO getAssetCategoryById(Long id);
+    List<AssetCategoryDTO> getAllAssetCategories();
+
+    // UPDATE
+    AssetCategoryDTO updateAssetCategory(Long id, AssetCategoryDTO assetCategoryRequest);
+
+    // DELETE (SOFT)
+    void deleteAssetCategoryById(Long id);
+}
