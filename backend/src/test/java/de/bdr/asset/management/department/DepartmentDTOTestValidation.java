c8f01a5b2b9e836dc45968957a063eb58b217d0c
Andela <amjogun@gmail.com>
2026-03-27 12:51:03 +0100

    Add validation tests for DepartmentDTO

---
+++ backend/src/test/java/de/bdr/asset/management/department/DepartmentDTOTestValidation.java
@@ -0,0 +1,63 @@
+package de.bdr.asset.management.department;
+
+import de.bdr.asset.management.user.department.DepartmentDTO;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validation;
+import jakarta.validation.Validator;
+import jakarta.validation.ValidatorFactory;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DepartmentDTOTestValidation {
+
+    private static Validator validator;
+
+    @BeforeAll
+    static void setValidation(){
+        ValidatorFactory factory=Validation.buildDefaultValidatorFactory();
+        validator=factory.getValidator();
+    }
+
+    private DepartmentDTO validDTO(){
+        return new DepartmentDTO(
+           1L,
+           "DEVOPS",
+           2L
+        );
+    }
+
+    private Set<ConstraintViolation<DepartmentDTO>> violationSet(String field, DepartmentDTO dto){
+        return validator.validateProperty(dto, field);
+    }
+
+    //All valid fields should produce no validation
+    @Test
+    void validDTO_shouldHaveNoViolations(){
+        assertThat(validator.validate(validDTO())).isEmpty();
+    }
+
+    //name
+
+    //Name is null
+    @Test
+    void nullName_shouldFailNotNull(){
+        DepartmentDTO dto=new DepartmentDTO(1L, null, 2L);
+        assertThat(validator.validate(dto)).anyMatch(v -> v.getPropertyPath().toString().equals("name"));
+    }
+
+    //managerId
+
+    //ManagerId is null
+    @Test
+    void nullManagerId_shouldFailNotNull(){
+        DepartmentDTO dto=new DepartmentDTO(1L, "DEVOPS", null);
+        assertThat(validator.validate(dto)).anyMatch(v -> v.getPropertyPath().toString().equals("managerId"));
+    }
+
+
+
+}
