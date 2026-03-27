dca991a6fdd173d184bfce701b03e5f43b6ac259
jakpetri <jakov.petric.student@gmail.com>
2026-03-27 13:53:06 +0100

    Updated AssetController to be in line with other controllers

--- backend/src/main/java/de/bdr/asset/management/asset/AssetController.java
+++ backend/src/main/java/de/bdr/asset/management/asset/AssetController.java
@@ -1,9 +1,18 @@
 package de.bdr.asset.management.asset;
 
-import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("api/v1/assets")
@@ -14,28 +23,35 @@ public class AssetController {
     public AssetController(AssetService assetService) {
         this.assetService = assetService;
     }
+    // CREATE
+    @PostMapping
+    public ResponseEntity<AssetDTO> createAsset(@Valid @RequestBody AssetDTO assetRequest) {
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(assetService.createAsset(assetRequest));
+    }
 
     //READ ALL
     @GetMapping
-    public List<AssetDTO> getAllAssets() {
-        return assetService.getAllAssets();
+    public ResponseEntity<List<AssetDTO>> getAllAssets() {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(assetService.getAllAssets());
     }
 
     // BY ID
     @GetMapping("/{id}")
-    public AssetDTO getAssetById(@PathVariable Long id) {
-        return assetService.getAssetById(id);
-    }
-
-    // CREATE
-    @PostMapping
-    public AssetDTO createAsset(@Valid @RequestBody AssetDTO dto) {
-        return assetService.createAsset(dto);
+    public ResponseEntity<AssetDTO> getAssetById(@PathVariable Long id) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(assetService.getAssetById(id));
     }
 
     // UPDATE
     @PutMapping("/{id}")
-    public AssetDTO updateAsset(@PathVariable Long id, @Valid @RequestBody AssetDTO dto) {
-        return assetService.updateAsset(id, dto);
+    public ResponseEntity<AssetDTO> updateAsset(@PathVariable Long id, @Valid @RequestBody AssetDTO assetRequest) {
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(assetService.updateAsset(id, assetRequest));
     }
 }
