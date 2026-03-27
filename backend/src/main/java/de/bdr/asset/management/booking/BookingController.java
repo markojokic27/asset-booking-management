2049831e5e9fd4358fd0826e7c09ed2782ccdc79
Josko Diko <josdjiko@student-gitlab.tn.internal>
2026-03-27 12:57:17 +0100

    Booking controller partially done

--- backend/src/main/java/de/bdr/asset/management/booking/BookingController.java
+++ backend/src/main/java/de/bdr/asset/management/booking/BookingController.java
@@ -5,6 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/bookings")
 public class BookingController {
@@ -23,4 +25,41 @@ public class BookingController {
 
         return new ResponseEntity<>(createdBooking, HttpStatus.CREATED);
     }
+
+    /** READ BY ID */
+    @GetMapping("/{d}")
+    public ResponseEntity<BookingDTO> getById(@PathVariable Long id) {
+
+        BookingDTO foundBooking = service.getBookingById(id);
+
+        return ResponseEntity.ok(foundBooking);
+    }
+
+    /** READ ALL */
+    @GetMapping
+    public ResponseEntity<List<BookingDTO>> getAll() {
+
+        List<BookingDTO> allBookings = service.getAllBookings();
+
+        return ResponseEntity.ok(allBookings);
+    }
+
+    /** UPDATE */
+    @PutMapping("/{id}")
+    public ResponseEntity<BookingDTO> update(@PathVariable Long id, @Valid @RequestBody BookingDTO request) {
+
+        BookingDTO updatedBooking = service.updateBooking(id, request);
+
+        return ResponseEntity.ok(updatedBooking);
+    }
+
+    /** Soft DELETE */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+
+        service.deleteBooking(id);
+
+        return ResponseEntity.noContent().build();
+    }
+
 }
