7cbc2d65872545add54f30440d20ca6ed9e8f408
jakpetri <jakov.petric.student@gmail.com>
2026-04-13 10:32:37 +0200

    Deleted the fallback from application.yaml file because there wa a cleaner solution for injecting properties for tests.

--- backend/src/test/java/de/bdr/asset/management/AssetBookingApplicationTests.java
+++ backend/src/test/java/de/bdr/asset/management/AssetBookingApplicationTests.java
@@ -4,9 +4,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "JWT_SECRET=1111111111111111111111111111111111111111",
+    "JWT_EXPIRY_SECONDS=3600",
+    "JWT_REFRESH_SECONDS=86400"
+})
 @ActiveProfiles("test")
 class AssetBookingApplicationTests {
+
     @Test
     void contextLoads() {
     }
