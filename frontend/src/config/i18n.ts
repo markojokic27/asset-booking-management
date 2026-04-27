973af00e4bc3c6605f4454ec82c747b6727c33c9
Nikolina Carevic <ncarevic231@gmail.com>
2026-04-27 12:34:02 +0200

    internacionalization i18next for MobileMenu

--- frontend/src/config/i18n.ts
+++ frontend/src/config/i18n.ts
@@ -1,10 +1,7 @@
 import i18next from 'i18next';
 import { initReactI18next } from 'react-i18next';
-import enCommon from './locales/en/common.json';
 import enLayout from './locales/en/layout.json';
-import hrCommon from './locales/hr/common.json';
 import hrLayout from './locales/hr/layout.json';
-import deCommon from './locales/de/common.json';
 import deLayout from './locales/de/layout.json';
 
 const savedLanguage = localStorage.getItem('language');
@@ -13,9 +10,9 @@ i18next.use(initReactI18next).init({
   lng: savedLanguage || 'hr',
   fallbackLng: 'en',
   resources: {
-    en: { translation: { common: enCommon, layout: enLayout } },
-    hr: { translation: { common: hrCommon, layout: hrLayout } },
-    de: { translation: { common: deCommon, layout: deLayout } },
+    en: { translation: { layout: enLayout } },
+    hr: { translation: { layout: hrLayout } },
+    de: { translation: { layout: deLayout } },
   },
   interpolation: {
     escapeValue: false,
--- frontend/src/types/i18next.d.ts
+++ frontend/src/types/i18next.d.ts
@@ -1,5 +1,4 @@
 import 'i18next';
-import enCommon from '../config/locales/en/common.json';
 import enLayout from '../config/locales/en/layout.json';
 
 declare module 'i18next' {
@@ -7,7 +6,6 @@ declare module 'i18next' {
     defaultNS: 'translation';
     resources: {
       translation: {
-        common: typeof enCommon;
         layout: typeof enLayout;
       };
     };
