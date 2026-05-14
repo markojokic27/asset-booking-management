f2a9169c31cbce10b9156913c24e571e493746f5
Marko Jokic <jokicmarko98@gmail.com>
2026-05-14 11:40:04 +0200

    add BookingByAsset page

--- frontend/src/pages/BookingByAsset.tsx
+++ frontend/src/pages/BookingByAsset.tsx
@@ -1,21 +1,43 @@
 // External packages
 import { useParams } from 'react-router-dom';
+import * as React from 'react';
 
 // Hooks
 import { useBookingsByAsset } from '../features/booking/hooks/useBookingByAsset';
 
 // Components
 import { LayoutColumn } from '../components/layout/Layout';
+import { FiltersBar } from '../features/booking/components/FilterBar';
+import { AvailabilityCalendar } from '../features/booking/components/AvailabilityCalendar';
+import { Button } from '../components/ui/Button';
 
 // Types
 import type { BookingDto } from '../features/booking/types';
+import type { AssetDto } from '../features/asset/types';
+import type { Filters } from '../features/booking/types';
+
+import { getAssetById } from '../features/asset/api/assetApi';
+
+const defaultFilters: Filters = {
+  search: '',
+  fromDate: '',
+  toDate: '',
+  fromHour: '',
+  toHour: '',
+};
 
 export default function BookingsByAsset() {
   const { assetId } = useParams();
+  const [filters, setFilters] = React.useState<Filters>(defaultFilters);
+
   const { bookings, loading, error } = useBookingsByAsset(assetId!);
+  const selectedFrom = `${filters.fromDate || '-'} ${filters.fromHour || ''}`;
+  const selectedTo = `${filters.toDate || '-'} ${filters.toHour || ''}`;
 
   if (loading) return <div>Loading...</div>;
   if (error) return <div>Greška</div>;
+
+  const asset = getAssetById(assetId!) as unknown as AssetDto;
 
   return (
     <LayoutColumn
@@ -24,11 +46,69 @@ export default function BookingsByAsset() {
       mdOffset={3}
       className="flex flex-col pt-35"
     >
-      {' '}
-      <h1>Bookings za asset {assetId}</h1>
-      {bookings.map((b: BookingDto) => (
-        <div key={b.id}>AAAAAAAAAAAAAAAAAA{b.assetId} </div>
-      ))}
+      <div>
+        <h2 className="mb-4 text-xl font-semibold">Book {asset.name}</h2>
+
+        <div className="mb-5 text-sm">
+          <p className="font-semibold">{asset.name}</p>
+          <p>Model: {asset.name ?? '-'}</p>
+          <p>Location: {asset.location ?? '-'}</p>
+        </div>
+
+        <div className="">
+          <div className="mb-6 flex w-full items-end justify-between">
+            <FiltersBar
+              variant={'DAY'}
+              filters={filters}
+              setFilters={setFilters}
+              showSearch={false}
+              className="grid-cols-1 sm:grid-cols-2 lg:max-w-[80%] lg:grid-cols-2"
+            />
+            <Button
+              variant="solid"
+              className="h-fit"
+              size="md"
+              onClick={() => {
+                console.log('BOOK', {
+                  asset,
+                  from: filters.fromDate,
+                  to: filters.toDate,
+                });
+              }}
+            >
+              Book
+            </Button>
+          </div>
+
+          {/* Implement checking asset status, if available user can book asset, else button book is disabled 
+          <div className="flex items-start gap-3 text-sm">
+            <span className="bg---color-status-active-bg flex h-5 w-5 items-center justify-center text-(--color-status-active-text)">
+              ✓
+            </span>
+
+            <p>
+              {/* If a date is not selected, the message is not displayed. Check availability - if available render AVAILABLE else UNAVAILABLE  }
+              {asset.name} is{' '}
+              <span className="rounded bg-(--color-status-active-bg) px-2 py-0.5 text-(--color-status-active-text)">
+                available
+              </span>{' '}
+              from {selectedFrom} to {selectedTo}
+            </p>
+          </div> */}
+        </div>
+
+        {/* Dohvatit bookinge odredenog asseta i posalt ih u events kako bi se prikazali u kalendaru*/}
+        <AvailabilityCalendar
+          events={[
+            {
+              id: '1',
+              title: `${asset.name} booked`,
+              start: '2026-04-29T10:00:00',
+              end: '2026-04-29T12:00:00',
+            },
+          ]}
+        />
+      </div>
     </LayoutColumn>
   );
 }
