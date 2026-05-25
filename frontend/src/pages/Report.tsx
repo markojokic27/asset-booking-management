76c1200cb8ba00b4fb36f2c1dd7816dfe1a41192
Marko Jokic <jokicmarko98@gmail.com>
2026-05-26 01:44:43 +0200

    fix layout on Report page

--- frontend/src/pages/Report.tsx
+++ frontend/src/pages/Report.tsx
@@ -1,8 +1,7 @@
 import { useTranslation } from 'react-i18next';
 
 // Components
-import { Layout, LayoutRow, LayoutColumn } from '../components/layout/Layout';
-import { Header } from '../components/layout/Header';
+import { LayoutColumn } from '../components/layout/Layout';
 
 import BookingStatusPie from '../features/report/components/BookingStatusPie';
 
@@ -11,34 +10,26 @@ export default function Report() {
 
   return (
     <>
-      <Header />
-
-      <Layout>
-        <LayoutRow>
-          <LayoutColumn
-            span={12}
-            mdSpan={9}
-            mdOffset={3}
-            className="flex min-h-screen flex-col pt-35 pb-10"
-          >
-            <div className="flex w-full flex-col gap-4">
-              <div className="flex flex-col gap-2">
-                <h1 className="text-3xl font-black tracking-widest text-black dark:text-white">
-                  {t('report.title')}
-                </h1>
-              </div>
-
-              <div className="h-px w-full bg-(--color-table-border)" />
-            </div>
-
-            <div className="mt-8 flex flex-col gap-6">
-              <div className="dark:bg-bg-dark rounded-2xl border border-(--color-table-border) p-6 shadow-sm dark:shadow-black/20">
-                <BookingStatusPie />
-              </div>
-            </div>
-          </LayoutColumn>
-        </LayoutRow>
-      </Layout>
+      <LayoutColumn
+        span={12}
+        mdSpan={9}
+        mdOffset={3}
+        className="flex min-h-screen flex-col pt-35 pb-10"
+      >
+        <div className="flex w-full flex-col gap-4">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-3xl font-black tracking-widest text-black dark:text-white">
+              {t('report.title')}
+            </h1>
+          </div>
+          <div className="h-px w-full bg-(--color-table-border)" />
+        </div>
+        <div className="mt-8 flex flex-col gap-6">
+          <div className="dark:bg-bg-dark rounded-2xl border border-(--color-table-border) p-6 shadow-sm dark:shadow-black/20">
+            <BookingStatusPie />
+          </div>
+        </div>
+      </LayoutColumn>
     </>
   );
 }
