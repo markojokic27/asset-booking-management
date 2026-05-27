856c57e18ceee142a09f8e9bbc1d993a0d93167a
monmajic <monikamajic03@gmail.com>
2026-05-27 12:33:18 +0200

    chore: move dependencies to version catalog

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -71,6 +71,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-    implementation("com.google.android.gms:play-services-code-scanner:16.1.0")
-    implementation("androidx.compose.material:material-icons-extended")
+    implementation(libs.play.services.code.scanner)
+    implementation(libs.androidx.compose.material.icons.extended)
 }
