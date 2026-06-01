a0494178d0d5a7ec9bfb68dbfd9b98c63dd3c7a2
davjezid <davidjezidzic@gmail.com>
2026-06-01 12:45:22 +0200

    feat: replace H2 with Testcontainers PostgreSQL for integration tests

--- backend/src/test/java/de/bdr/asset/management/AssetBookingApplicationTests.java
+++ backend/src/test/java/de/bdr/asset/management/AssetBookingApplicationTests.java
@@ -5,12 +5,12 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest(properties = {
-    "JWT_SECRET=1111111111111111111111111111111111111111",
-    "JWT_EXPIRY_SECONDS=3600",
-    "JWT_REFRESH_SECONDS=86400"
+        "JWT_SECRET=1111111111111111111111111111111111111111",
+        "JWT_EXPIRY_SECONDS=3600",
+        "JWT_REFRESH_SECONDS=86400"
 })
 @ActiveProfiles("test")
-class AssetBookingApplicationTests {
+class AssetBookingApplicationTests extends AbstractIntegrationTest {
 
     @Test
     void contextLoads() {
