20d3f5d3b10242bf5ab48e3786c2e48e7c2115be
jakpetri <jakov.petric.student@gmail.com>
2026-06-02 10:55:17 +0200

    Just added comment for ENV variable for Grafana and updated wording for DELETE on Asset Category

--- backend/src/main/java/de/bdr/asset/management/assetcategory/AssetCategoryController.java
+++ backend/src/main/java/de/bdr/asset/management/assetcategory/AssetCategoryController.java
@@ -1,8 +1,5 @@
 package de.bdr.asset.management.assetcategory;
 
-import de.bdr.asset.management.assetcategory.dto.AssetCategoryRequestDTO;
-import de.bdr.asset.management.assetcategory.dto.AssetCategoryResponseDTO;
-import de.bdr.asset.management.assetcategory.dto.AssetCategoryUpdateRequestDTO;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,13 +8,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import de.bdr.asset.management.assetcategory.dto.AssetCategoryRequestDTO;
+import de.bdr.asset.management.assetcategory.dto.AssetCategoryResponseDTO;
+import de.bdr.asset.management.assetcategory.dto.AssetCategoryUpdateRequestDTO;
 import de.bdr.asset.management.core.exception.DuplicateResourceException;
 import de.bdr.asset.management.core.exception.ResourceNotFoundException;
 import io.swagger.v3.oas.annotations.Operation;
@@ -98,7 +98,7 @@ public class AssetCategoryController {
         return ResponseEntity.ok(updatedCategory);
     }
 
-    /** Soft DELETE */
+    /** DELETE if no assets and bookings for category */
     @Operation(summary = "Soft delete asset category", description = "Only available to users with role: ADMIN.")
     @SecurityRequirement(name = "Bearer Authentication")
     @PreAuthorize("hasRole('ADMIN')")
