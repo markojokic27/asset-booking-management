c1c2fed02f9700581fb0235aacf51058d9eff604
monmajic <monikamajic03@gmail.com>
2026-06-03 11:07:44 +0200

    test: add LoginScreen UI tests

--- mobile/android/app/build.gradle.kts
+++ mobile/android/app/build.gradle.kts
@@ -66,6 +66,7 @@ dependencies {
     implementation(libs.retrofit.kotlinx.serialization.converter)
     testImplementation(libs.junit)
     testImplementation(libs.mockito.core)
+    testImplementation(libs.kotlinx.coroutines.test)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
